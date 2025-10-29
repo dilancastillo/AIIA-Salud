24d6eb813c43080f62048470e86c72d2e2309e22
dilancastillo <dilancastillom@gmail.com>
2025-10-28 20:49:34 -0500

    Inicial commit: AIIA App

---
+++ app/src/main/java/com/aiia/hospital/aiia/CoverActivity.kt
@@ -0,0 +1,20 @@
+package com.aiia.hospital.aiia
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aiia.hospital.aiia.databinding.ActivityCoverBinding
+
+class CoverActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCoverBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCoverBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEmpezar.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/aiia/hospital/aiia/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.aiia.hospital.aiia
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.robotemi.sdk.Robot
+import com.robotemi.sdk.TtsRequest
+import com.aiia.hospital.aiia.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), Robot.TtsListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    override fun onTtsStatusChanged(ttsRequest: TtsRequest) {
+
+    }
+}
